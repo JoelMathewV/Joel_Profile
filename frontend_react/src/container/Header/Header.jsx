51815cec90daf31ab1ebf7c35dd40f4e2d1edfaf
Joel Mathew V <joelmathewv0819@gmail.com>
2022-08-03 18:25:04 +0530

    navbar created

---
+++ frontend_react/src/container/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.scss';
+import {motion} from 'framer-motion';
+import { images } from '../../constants';
+
+const Header = () => {
+  return (
+    <div className='app__header app__flex'>
+      
+
+    </div>
+  )
+}
+
+export default Header
